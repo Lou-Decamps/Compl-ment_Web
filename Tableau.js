125a36f8d81920562d71163a1153562e4a00d4c3
Lou-Decamps <lou.decamps@etu.univ-amu.fr>
2026-01-22 12:16:01 +0100

    exo 3 a finir et exo 4

--- Tableau.js
+++ Tableau.js
@@ -9,6 +9,31 @@ function Tableau() {
     };
     return this.tableau;
 }
-console.log(Tableau());
 
-//Exo 2
+tab = Tableau();
+console.log(tab);
+
+//Exo 3
+let intervalId;
+
+function stopVal() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+function tabNum() {
+    for (let i = 0; i < tab.length; i++) {
+        const intervalId =  setInterval(tab[i], 1000);
+    }
+    stopVal();
+    const intervalID = setInterval(myCallback, 500, "Parameter 1", "Parameter 2");
+
+    function myCallback(a, b) {
+        // Your code here
+        // Parameters are purely optional.
+        console.log(a);
+        console.log(b);
+    }
+}
+
+document.getElementById("start").addEventListener("click", tabNum);
