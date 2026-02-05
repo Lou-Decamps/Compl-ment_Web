d31f2885d18acbf2e169e997c711f970092d5bb5
Lou-Decamps <lou.decamps@etu.univ-amu.fr>
2026-02-05 15:58:14 +0100

    exo 3.5 et 6

--- Tableau.js
+++ Tableau.js
@@ -1,9 +1,10 @@
 // Exo 1
 const tab = [];
 let i = 0
-delay = 2000;
-zone = document.getElementById("valeur");
-function Tableau() {
+const delay = 2000;
+
+
+function Tableau(min, max) {
     min = Math.ceil(-10);
     max = Math.floor(40);
 
@@ -15,21 +16,53 @@ function Tableau() {
 
 //Exo 3
 let intervalId;
+const zone = document.getElementById("valeur");
+var text = document.getElementById("text");
+//const text = document.getElementById("text");
 
+// function showtabNum() {
+//     Tableau(tab);
+//     if (i< tab.length)
+//     {
+//         zone.textContent = tab[i];
+//         i++;
+//         setInterval(showtabNum, delay);
+//     }
+//     else
+//     {
+//         clearInterval(this.delay)
+//         clearInterval(intervalId);
+//     }
+// }
+setInterval(function ()
+{
+    const val = tab[i];
+    zone.textContent = val;
+    color(val);
+    i++;
+}, delay);
 
-function showtabNum() {
-    Tableau(tab);
-    if (i< tab.length)
-    {
-        zone.textContent = tab[i];
-        i++;
-        setInterval(showtabNum, delay);
+function color(num) {
+    zone.classList.remove(
+        "classe-1", "classe-2", "classe-3", "classe-4"
+    )
+
+    if (num <= 0) {
+        document.getElementById("text").textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
+        zone.classList.add("classe-1");
     }
-    else
-    {
-        clearInterval(this.delay)
-        clearInterval(intervalId);
+    else if (num >= 0 && num < 20) {
+        document.getElementById("text").textContent = "";
+        zone.classList.add("classe-2");
+    }
+    else if (num >= 20 && num < 30) {
+        document.getElementById("text").textContent = "";
+        zone.classList.add("classe-3");
+    }
+    else if (num >= 30 && num <= 40) {
+        document.getElementById("text").textContent = "Caliente ! Vamos a la playa, ho hoho hoho !!";
+        zone.classList.add("classe-4");
     }
 }
+
 Tableau(tab);
-const intervalID = setInterval(showtabNum, delay);
